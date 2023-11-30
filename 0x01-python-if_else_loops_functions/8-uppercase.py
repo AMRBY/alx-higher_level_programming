3838ec68bb41c71aa2bd40aa5610b652a782b4d1
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-11-30 00:47:54 +0000

    mandatory

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str):
+        x = str[i]
+        if x > 96 and x < 123
+            up[i] = chr(x - 32)
+        else:
+            up[i] = str[i]
+    return up
