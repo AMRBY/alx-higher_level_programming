5953f7133bef03a0aa87780ca650e7994c10de7a
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-02 11:20:33 +0000

    mandatory

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 1:
+    from sys import argv
+    if len(argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) == 2:
+    elif len(argv) == 2:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         i = 1
-        while i < len(sys.argv):
-            print("{}: {}".format(i, sys.argv[i]))
+        while i < len(argv):
+            print("{}: {}".format(i, argv[i]))
             i += 1
