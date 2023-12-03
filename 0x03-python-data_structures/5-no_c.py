5b99ba2f814d27f2e1e9cb1692387e13b68c7305
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-03 15:20:28 +0000

    mandatory

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_list = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            my_list += i
+            j += 1
+    return my_list
