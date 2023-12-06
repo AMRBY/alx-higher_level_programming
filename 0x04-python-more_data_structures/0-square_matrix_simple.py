2128cd10280d99eff6171c37c63057e16a0fc5e9
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-06 22:39:20 +0000

    mandatory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     mat = []
-    for i in range(0,3):
+    for i in range(0, 3):
         c = []
         for x in matrix[i]:
             c.append(x*x)
         mat.append(c)
-    return mat 
+    return mat
