dc88a1dd7b6f18a25fa622aaa13e0c6b72d22274
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-06 23:28:13 +0000

    mandatory

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new = my_list.copy()
     for x in new:
         if x == search:
-            pos = new.index(search)
+            pos = new.index(x)
             del(new[pos])
             new.insert(pos, replace)
     return new
