de66ac907053fd93e6d1871787de219b0469f989
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-06 23:02:16 +0000

    mandatory

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list.copy()
-    for x in my_list:
+    for x in new:
         if x == search:
             pos = new.index(search)
             del(new[pos])
