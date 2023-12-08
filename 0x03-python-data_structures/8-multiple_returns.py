3a79657a71bad7c64df1ea1ac8aad5243cef0e67
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-08 14:20:52 +0000

    mandatory

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     lent = len(sentence)
-    sen = sentence[0]
+    if lent > 0:
+        sen = sentence[0]
+    else:
+        sen = None
     return (lent, sen)
