55f216e4893dc307ea3a2f7b5dc61fa5583a0e36
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-08 14:50:35 +0000

    mandatory

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,9 @@ def divisible_by_2(my_list=[]):
     i = 0
     new = []
     while i < len(my_list):
-        if (my_list[i] % 2) == 1:
-            new.append('')
+        if my_list[i] % 2 == 1:
+            new.append(0)
         else:
-            new.append('True')
+            new.append(1)
         i += 1
     return new
