cba4c17964c7080226989463f4dd4d040d243e45
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-19 21:30:29 +0000

    mandatory

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    j = 0
     for i in range(0, x):
         try:
             print('{}'.format(my_list[i]), end='')
-            i += 1
+            j += 1
         except IndexError:
             break
     print('')
-    return i
+    return j
