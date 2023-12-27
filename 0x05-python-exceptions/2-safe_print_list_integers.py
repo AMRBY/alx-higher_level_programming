4d53eb4bb5b760db668422fd725c659ac5c9bd46
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-27 22:41:09 +0000

    mandatory

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    for value in range(0, x):
+        try:
+            print("{:d}".format(my_list[value], end=""))
+            i += 1
+        except (IndexError, ValueError, TypeError):
+            pass
+    return i
