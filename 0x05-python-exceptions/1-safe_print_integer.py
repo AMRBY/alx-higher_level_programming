c68b1768f39ff1fec207c3a62475f1e82bbdfa38
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-27 15:13:59 +0000

    mandatory

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return 1
-    except (TypeError, ValueError):
+    except (ValueError, TypeError):
         return 0
