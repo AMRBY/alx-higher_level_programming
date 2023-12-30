ca09376581efefb42d112c2ed72fd745633d4d8b
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-30 15:28:40 +0000

    mandatory

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Square is an empty class
+    __init__: method to initialize a parameter
+    Attributes:
+        __size: __size of the square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        pass
+
+    def area(self):
+        """"area is a method to calculate an area of a square"""
+        return (self.__size * self.__size)
