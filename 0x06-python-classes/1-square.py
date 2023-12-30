e6c961337a206cc982b6fc3ee7a28239b8ad1c49
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-30 10:25:12 +0000

    mandatory

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module for square"""
+
+
+class Square:
+    """Square is an empty class
+    __init__: method to initialize a parameter
+    Attributes:
+        __size: __size of the square"""
+    def __init__(self, __size=None):
+        self.__size = __size
+        pass
