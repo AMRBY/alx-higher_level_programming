31b1eb7a05b7c055e193c24ae978859154e7004b
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-03 19:04:35 +0000

    mandatory

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""0-add_integer is a a module to add two inetgers
+'a' and 'b' must be inetegr or float
+they must be casted to int if float
+raise an error if not
+return (a + b)"""
+
+
 def add_integer(a, b=98):
+    """add_integer: function to add two integers
+    a: first int/float
+    b: second int/float"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
