fbd811d21171d0bcee746a042db7ca9ca214d333
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-05 23:06:57 +0000

    mandatory

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""this is the __main__ module
+to define a class
+"""
+
+
+class Rectangle:
+    """this is a Rectangle class"""
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """"initiate wen creating an object
+        Args:
+            width: attribute (int)
+            height: attribute (int)"""
+        self.width = width
+        self.height = height
+        number_of_instances += 1
+
+    @property
+    def width(self):
+        """width is a getter/setter methods"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height is a getter/setter methods"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (2 * (self.__height + self.__width))
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        jiji = str("#" * self.__width + "\n") * self.__height
+        return jiji.strip()
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        number_of_instances -= 1
+        print("Bye rectangle...")
