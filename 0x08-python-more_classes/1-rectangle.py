4afd047f3a58b010c83980a8d9825d9e21d640b9
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-05 19:50:59 +0000

    mandatory

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
         Args:
             width: attribute (int)
             height: attribute (int)"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
