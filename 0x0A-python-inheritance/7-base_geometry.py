605cb5ada84db0d76a6d10a4c269b85682c57d85
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 18:29:14 +0000

    mandatory

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,13 +6,14 @@
 
 class BaseGeometry:
     """BaseGeometry is a class, it sort a list"""
+
     def area(self):
-    """area is a class, it sort a list"""
+        """area is a class, it sort a list"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-    """integer is a class, it sort a list"""
-        if not type(self.value) is int:
+        """integer is a class, it sort a list"""
+        if not type(value) is int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
