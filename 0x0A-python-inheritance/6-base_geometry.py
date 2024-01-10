7b887720b683ee9e64cdd8a71899b188d81062a5
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 17:49:36 +0000

    mandatory

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" __main__ module
+
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry is a class, it sort a list"""
+    def area(self):
+        raise Exception("area() is not implemented")
