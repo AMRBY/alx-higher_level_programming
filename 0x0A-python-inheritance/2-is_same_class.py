a018c162f373171a4d4de2fd1605c1bee701a9af
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 16:52:37 +0000

    mandatory

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" __main__ module
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """print_sorted is a function, it sort a list"""
+    if obj.__class__.__name__ == a_class
+        return True
+    else:
+        return False
