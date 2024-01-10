85c973180bc97698f590af8c1dc2d003314bf107
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 14:07:50 +0000

    mandatory

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+""" __main__ module
+
+"""
+
+
 def lookup(obj):
+    """lookup is a function that returns modules and attributes
+    args:
+        obj: is the object"""
     return dir(obj)
