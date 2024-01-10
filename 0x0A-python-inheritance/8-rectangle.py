5ccfc5075fde08aa58ede9a974caef1a77533b03
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 22:42:18 +0000

    mandatory

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(BaseGeometry):
             width: integer
             height: integer
         """
-        self.__width = width
-        self.__height = height
         super().integer_validator("width", self.__width)
+        self.__width = width
         super().integer_validator("height", self.__height)
+        self.__height = height
