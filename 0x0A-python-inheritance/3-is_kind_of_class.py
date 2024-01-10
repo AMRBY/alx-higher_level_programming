e3bf9c8cd118f120beb648d9057364287aef5b79
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 17:24:03 +0000

    mandatory

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" __main__ module
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """print_sorted is a function, it sort a list"""
+    if isinstance(obj, a_class)
+        return True
+    else:
+        return False
