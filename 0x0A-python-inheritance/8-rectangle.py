33de069e145d2ba75d5594d829d427c5ecb8f0b8
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-10 22:48:58 +0000

    mandatory

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,24 +4,7 @@
 """
 
 
-class BaseGeometry:
-    """BaseGeometry is a class, it sort a list"""
-
-    def area(self):
-        """area is a class, it sort a list"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer is a class, it sort a list
-        args:
-            name: string
-            value: inetger"""
-
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle is a class, it sort a list"""
