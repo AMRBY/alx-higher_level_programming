38c8430d23fb1210b2baec6b798bf74c8dbe4215
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-11 18:33:15 +0000

    mandatory

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this is I/O file functions
+"""
+
+
+def write_file(filename="", text=""):
+    """read_file is a function to read a file
+    args:
+        filename: is a filename
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        print(f.write(text))
