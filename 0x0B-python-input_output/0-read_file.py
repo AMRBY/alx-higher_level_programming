6a0bebc104f8ed51278a242c51484b6a669fa6ea
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-11 18:02:43 +0000

    mandatory

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,9 @@
 
 
 def read_file(filename=""):
-    """read_file is a function to read a file"""
+    """read_file is a function to read a file
+    args:
+        filename: is a filename
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read())
