769d8b74b1294190e41f833b5be263fbc047357a
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-11 18:35:08 +0000

    mandatory

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
         filename: is a filename
     """
     with open(filename, 'w', encoding='utf-8') as f:
-        print(f.write(text))
+        return f.write(text)
