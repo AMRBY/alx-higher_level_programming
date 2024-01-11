d065179616406c45652c4e8893d32f32f7a80521
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-11 18:47:46 +0000

    mandatory

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this is I/O file functions
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """read_file is a function to read a file
+    args:
+        filename: is a filename
+    """
+    return json.dumps(my_obj)
