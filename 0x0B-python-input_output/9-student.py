0f02653072b913258d1ef18c895feefbc958f340
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-12 00:12:15 +0000

    mandatory

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" this is I/O file functions
+"""
+
+
+class Student:
+    """read_file is a function to read a file
+    args:
+        filename: is a filename
+    """
+    first_name
+    last_name
+    age
+    def __init__(self, first_name, last_name, age):
+        """read_file is a function to read a file"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """read_file is a function to read a file"""
+        return self.__dict__
