ed81a18a342bf94610e8efd215de444312a0fbc1
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-01-12 00:14:51 +0000

    mandatory

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,15 +4,14 @@
 
 
 class Student:
-    """read_file is a function to read a file
-    args:
-        filename: is a filename
-    """
-    first_name
-    last_name
-    age
+    """read_file is a function to read a file"""
     def __init__(self, first_name, last_name, age):
-        """read_file is a function to read a file"""
+        """read_file is a function to read a file
+    args:
+        first_name: is a filename
+        last_name: is a str
+        age: is int
+    """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
