d5c350b595f5f47d529a69673e8fb981ee548e73
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-02-14 23:47:06 +0000

    mandatory

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
