dc2559aaa33846ca2de27738042e5ff262c11301
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-02-15 19:24:49 +0000

    mandatory

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- join left with where
-SELECT * FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL;
+SELECT * FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
