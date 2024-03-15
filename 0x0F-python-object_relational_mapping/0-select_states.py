c8fa27c1113bf90d2393946066402da0c8391716
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-15 18:01:27 +0000

    mandatory

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+"""
+This is sql/pyton tests
+"""
 import MySQLdb
 import sys
+
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
