afdd82906d2f3f95eb692f4fb607eec97d3c371f
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-15 21:57:25 +0000

    mandatory

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that list all `states` starting with 'N' from the database `hbtn_0e_0_usa`
+list all `states` starting with 'N' from the database `hbtn_0e_0_usa`
 """
 import MySQLdb
 from sys import argv
@@ -13,7 +13,8 @@ def main():
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE \
+                 BINARY 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
