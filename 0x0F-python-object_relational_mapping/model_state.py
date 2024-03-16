496d2413dcf28891160efe1250623d43fbd9f6ba
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-16 23:43:17 +0000

    mandatory

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this is ORM objects mapped to tables
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class mapped with states table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
