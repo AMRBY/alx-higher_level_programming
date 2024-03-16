1c425fdd6a5a143978e48e91f43859861cfc086b
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-16 23:44:13 +0000

    mandatory

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,12 +2,11 @@
 """
 this is ORM objects mapped to tables
 """
-
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class State(Base):
     """
