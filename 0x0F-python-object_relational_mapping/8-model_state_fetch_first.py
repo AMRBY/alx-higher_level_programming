44345951f63ae7bc0d62e08e280661042bda21cc
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-17 14:55:38 +0000

    mandatory

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
+    state = session.query(State).order_by(State.id).first():
         if state:
             print("{}: {}".format(state.id, state.name))
             break
