bc9e9cb1f1fb042645301a4497e655b119048398
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-17 22:37:15 +0000

    mandatory

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+show all rows in a table
+"""
+from sqlalchemy.orm import Session
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    louisiana = State(name='Louisiana')
+    session.add(louisiana)
+    session.commit()
+    session.close()
