0282d6e1162d22462e98f3cd794dad3ea27895bb
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-27 23:19:32 +0000

    mandatory

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #display methods
-curl -s -X OPTIONS "$1" | grep "Allow" | cut -d" " -f2
+#curl -s -X OPTIONS "$1" | grep "Allow" | cut -d" " -f2
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
