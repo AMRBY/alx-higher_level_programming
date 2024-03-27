5fc73c0454453f16b95cc80d25c0dd7b6a056d1d
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-27 23:15:51 +0000

    mandatory

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #display methods
-curl -sLX POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
+curl -sLX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
