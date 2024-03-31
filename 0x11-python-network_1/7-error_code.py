e2909d1107c3b7cf8e853efaf8dd652c7d36eaee
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-31 18:08:56 +0000

    mandatory

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a program to get status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code > 400:
+        print(f"Error code: {response.status_code}")
