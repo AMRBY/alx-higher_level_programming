ae04dd39a9981ca598d9c8438aaeefcc1c0614f9
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-31 17:46:14 +0000

    mandatory

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is a program to get status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers['X-Request-Id'])
