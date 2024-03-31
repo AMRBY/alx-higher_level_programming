ab715b7d0951442d27b40e979d39deda46399c1f
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-31 15:40:28 +0000

    mandatory

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@ import urllib.request
 import urllib.parse
 import sys
 
-
-obj = urllib.request.Request(sys.argv[1], method='GET')
-with urllib.request.urlopen(obj) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    obj = urllib.request.Request(sys.argv[1], method='GET')
+    with urllib.request.urlopen(obj) as response:
+        print(response.headers.get('X-Request-Id'))
