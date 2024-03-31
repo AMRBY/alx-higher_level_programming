500da3eb2c7fbabcc5e5e32d9976f168d9cf62d8
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-31 18:02:54 +0000

    mandatory

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this is a program to get status"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    email = sys.argv[2]
+    req = requests.post(sys.argv[1], data={'email': email})
+    response = requests.get(req)
+    print(response.text)
