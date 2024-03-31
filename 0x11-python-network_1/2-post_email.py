f30524477241056f133ce6411c168454962794ec
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-31 16:41:01 +0000

    mandatory

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,8 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    email = urllib.parse.urlencode({'email': sys.argv[2]})
-    email = email.encode('utf-8')
-    with urllib.request.urlopen(sys.argv[1], email) as response:
+    email = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], email)
+
+    with urllib.request.urlopen(req) as response:
         print(response.decode('utf-8'))
