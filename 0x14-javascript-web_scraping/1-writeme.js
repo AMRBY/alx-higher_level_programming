0b8beddb0c58878b209cdc375d2f15c69363113b
AMR BY <bouyoussefamr@gmail.com>
2024-05-05 20:44:59 +0000

    mandatory

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,4 @@
+
 #!/usr/bin/node
 // read a file
 
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// read a file
+
+const request = require('request');
+const { argv } = require('node:process');
+
+const req = new Request(request);
+const reponse = fetch(req);
