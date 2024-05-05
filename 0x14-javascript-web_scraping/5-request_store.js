4842689d4419ac29f130cccb898519920f41dfee
AMR BY <bouyoussefamr@gmail.com>
2024-05-05 22:53:39 +0000

    mandatory

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// read a file
+
+const file = require('fs');
+const request = require('request');
+const { argv } = require('node:process');
+request(argv[2],
+  function (error, response, body) {
+    if (!error) {
+      file.writeFile(argv[3], body,
+        function (err) {
+          if (err) {
+            return console.log(err);
+          }
+        });
+    }
+  }
+);
