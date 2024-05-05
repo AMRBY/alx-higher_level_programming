cc7a79a770dc53fd7133f0cebd132599f689b199
AMR BY <bouyoussefamr@gmail.com>
2024-05-05 23:31:28 +0000

    mandatory

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,22 @@
 
 const request = require('request');
 const { argv } = require('node:process');
+const dict = {};
 request(argv[2],
   function (error, response, body) {
     if (!error) {
       for (let j = 1; j < 11; j++) {
         let nb = 0;
         for (let i = 0; i < JSON.parse(body).length; i++) {
-          if (JSON.parse(body).userId == j) {
-	    if (JSON.parse(body).completed === "true") {
-            	nb++;
-	    }
+          if (JSON.parse(body)[i].userId === j) {
+            if (JSON.parse(body)[i].completed) {
+              nb++;
+            }
           }
         }
-        console.log("'%d': %d", j, nb):
+        dict[j - 1] = nb;
       }
+      console.log(dict);
     }
   }
 );
