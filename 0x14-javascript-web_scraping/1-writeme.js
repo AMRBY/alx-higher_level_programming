9770573c043bd6b60f576f25863aabffcac9826e
AMR BY <bouyoussefamr@gmail.com>
2024-05-05 20:33:03 +0000

    mandatory

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// read a file
+
+const file = require('fs');
+const { argv } = require('node:process');
+file.writeFile(argv[2], argv[3],
+  function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
